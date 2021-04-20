980bd950eb3029a70bf91e959dea358b263ae121
Ariel Abimelek Monterrubio Reyes <arielito581@gmail.com>
2021-04-20 14:05:25 -0500

    Se agrega CONTADOR a SuperHeores en appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
-import { HeroeComponent } from './heroes/heroe/heroe.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
-import { ListadoComponent } from './heroes/listado/listado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroeComponent,
-    ListadoComponent,
+    
   ],
 
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
